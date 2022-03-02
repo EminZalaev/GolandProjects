ec0b320856b2e464dd9bea02920600e23325af1d
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-02 23:52:40 +0300

    dev12

--- dev12/main.go
+++ dev12/main.go
@@ -1,1 +1,26 @@
-package dev12
+// Имеется последовательность строк - (cat, cat, dog, cat, tree) создать для нее собственное множество.
+package main
+
+import "fmt"
+
+func newSet(a []string) []string {
+	createdSet := make(map[string]bool)
+
+	for _, i := range a {
+		createdSet[i] = true
+	}
+
+	result := make([]string, 0, len(createdSet))
+	for i := range createdSet {
+		result = append(result, i)
+	}
+
+	return result
+}
+
+func main() {
+	stringSubsequence := []string{"cat", "cat", "dog", "cat", "tree"}
+
+	out := newSet(stringSubsequence)
+	fmt.Println(out)
+}
