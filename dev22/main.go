7b729c7aba4c015158eab5e5cc29b082fc2c29b5
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-02 23:45:02 +0300

    dev22

--- dev22/main.go
+++ dev22/main.go
@@ -1,1 +1,18 @@
-package dev22
+//Разработать программу,
+//которая перемножает, делит, складывает, вычитает две числовых переменных a,b, значение которых > 2^20.
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	a := big.NewInt(222222222)
+	b := big.NewInt(111111111)
+
+	fmt.Println(new(big.Int).Add(a, b))
+	fmt.Println(new(big.Int).Sub(a, b))
+	fmt.Println(new(big.Int).Mul(a, b))
+	fmt.Println(new(big.Int).Div(a, b))
+}
