53e06136deff90ea1bea37d87b4375d9b2d331c0
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-03 00:18:42 +0300

    dev25

--- dev25/main.go
+++ dev25/main.go
@@ -1,1 +1,34 @@
-package dev25
+//Реализовать собственную функцию sleep.
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func mySleep(ch <-chan string, d time.Duration) {
+
+	// Select statement
+	select {
+
+	// Using case statement to receive
+	// or send operation on channel
+	case output := <-ch:
+		fmt.Println(output)
+
+	// Calling After() method with its
+	// parameter
+	case <-time.After(d):
+
+		// Printed after 5 seconds
+		fmt.Println("Its timeout..")
+	}
+}
+
+func main() {
+	// Creating a channel
+	// Using make keyword
+	channel := make(chan string, 2)
+	fmt.Println("Start wait")
+	mySleep(channel, time.Second*3)
+}
