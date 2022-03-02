b748458b626cef3a3c1eac6f6cea9d4b6e652ed3
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-03 00:00:15 +0300

    dev18

--- dev18/main.go
+++ dev18/main.go
@@ -1,1 +1,36 @@
-package dev18
+//Реализовать структуру-счетчик, которая будет инкрементироваться в конкурентной среде.
+//По завершению программа должна выводить итоговое значение счетчика.
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Increment struct {
+	count int
+	lock  sync.Mutex
+}
+
+func (i *Increment) Increase() {
+	i.lock.Lock()
+	i.count++
+	defer i.lock.Unlock()
+}
+
+func main() {
+	increment := Increment{count: 0}
+	var wg sync.WaitGroup
+	n := 100000
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			for i := 0; i < 10; i++ {
+				increment.Increase()
+			}
+			defer wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(increment.count)
+}
