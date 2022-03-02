74ef6fdc02b6a9264d3e3e026f771865a8f6cb84
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-02 19:23:55 +0300

    dev15

--- dev15/main.go
+++ dev15/main.go
@@ -1,1 +1,31 @@
-package dev15
+//К каким негативным последствиям может привести данный фрагмент кода, и как это исправить?
+//Приведите корректный пример реализации.
+//
+//var justString string
+//func someFunc() {
+//  v := createHugeString(1 << 10)
+//  justString = v[:100]
+//}
+//
+//func main() {
+//  someFunc()
+//}
+
+//1)Напрягает то, что переменная justString глобальная, возможны проблемы
+//со сборщиком мусора
+//2)К нам прилетает строка 2^10, а нам нужно только 100, поэтому копируем срез
+//3) по хорошему строку ставим в []rune из за проблем с uft
+
+//package main
+
+//var justString string
+//func someFunc() {
+//	v := createHugeString(1 << 10)
+//	c := make([]byte, 0, len(v))
+//	copy(c, v)
+//	justString = string(c)
+//}
+//
+//func main() {
+//	someFunc()
+//}
