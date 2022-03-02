8b290e1c8d88058145304cec1ee3833d5f054313
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-02 18:36:42 +0300

    17 and 23

--- dev17/main.go
+++ dev17/main.go
@@ -1,1 +1,38 @@
-package dev17
+//Реализовать бинарный поиск встроенными методами языка.
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func binarSearch(array []int, item int) (int, error) {
+	lowKey := 0
+	highKey := len(array) - 1
+	for lowKey <= highKey {
+		mid := (lowKey + highKey) / 2
+		guess := array[mid]
+		if guess == item {
+			return mid, nil
+		} else if guess > item {
+			highKey = mid - 1
+		} else {
+			lowKey = mid + 1
+		}
+	}
+
+	return 0, errors.New("No number in array")
+}
+
+func main() {
+	// Массив должен быть отсортирован!
+	// Худшее время - логарифмическое O(log n); лучшее - константное O(1)
+	arr := []int{0, 5, 7, 11, 15, 18, 22}
+
+	result, err := binarSearch(arr, 5)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(result)
+}
--- dev23/main.go
+++ dev23/main.go
@@ -1,1 +1,13 @@
-package dev23
+// Удалить i-ый элемент из слайса.
+package main
+
+import "fmt"
+
+func removeElementFromSlice(arr []int, index int) []int {
+	return append(arr[0:index], arr[index+1:]...) //при помощи append
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(removeElementFromSlice(arr, 3))
+}
