cf4baec981be1b2086411a3bb7e8eb3b30a48c81
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-02 18:59:34 +0300

    dev13

--- dev13/main.go
+++ dev13/main.go
@@ -1,1 +1,13 @@
-package dev13
+//Поменять местами два числа без создания временной переменной.
+
+package main
+
+import "fmt"
+
+func main() {
+	a := 5
+	b := 6
+	fmt.Println(a, b)
+	a, b = b, a //в Go можно менят местами две перенменные не создавая третью
+	fmt.Println(a, b)
+}
