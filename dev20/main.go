2157763d305c7999b1b8474b19d245d9ed374656
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-02 21:17:09 +0300

    dev20

--- dev20/main.go
+++ dev20/main.go
@@ -1,1 +1,25 @@
-package dev20
+//Разработать программу, которая переворачивает слова в строке.
+//Пример: «snow dog sun — sun dog snow».
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func reversedWords(str string) string {
+	words := strings.Fields(str)
+
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+	return strings.Join(words, " ")
+}
+
+func main() {
+	words := "snow dog sun"
+	slova := "снег собака солнце"
+
+	fmt.Println(reversedWords(words))
+	fmt.Println(reversedWords(slova))
+}
