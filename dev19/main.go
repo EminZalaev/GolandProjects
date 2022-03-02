419771037b6b6ecb6699f6b0e22d74b7222870fb
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-02 21:09:02 +0300

    dev19

--- dev19/main.go
+++ dev19/main.go
@@ -1,1 +1,21 @@
-package dev19
+//Разработать программу, которая переворачивает
+//подаваемую на ход строку (например: «главрыба — абырвалг»). Символы могут быть unicode.
+package main
+
+import "fmt"
+
+func reversedString(strings string) string {
+	stringRune := []rune(strings)
+	reverseRune := make([]rune, len(stringRune))
+	for i := 0; i < len(stringRune); i++ {
+		reverseRune[i] = stringRune[len(stringRune)-1-i]
+	}
+	return string(reverseRune)
+}
+
+func main() {
+	firstString := "s''123;asd;aweqd"
+	secondString := "ввыфвфы1231323э123э"
+	fmt.Println(reversedString(firstString))
+	fmt.Println(reversedString(secondString))
+}
