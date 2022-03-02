4fc7d97fcfb70754d1fe3ff384249e4a792723e1
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-02 20:53:56 +0300

    dev11

--- dev11/main.go
+++ dev11/main.go
@@ -1,1 +1,38 @@
-package dev11
+//Реализовать пересечение двух неупорядоченных множеств.
+package main
+
+import (
+	"fmt"
+)
+
+func getIntersection(firstArr []string, secondArr []string) []string {
+	set1 := make(map[string]byte)
+
+	for _, k := range firstArr {
+		set1[k] += 1
+		fmt.Println(set1)
+	}
+
+	for _, k := range secondArr {
+		set1[k] += 2
+		fmt.Println(set1)
+	}
+
+	result := make([]string, 0, len(firstArr))
+
+	for k, v := range set1 {
+		if v == 3 {
+			result = append(result, k)
+		}
+	}
+
+	return result
+}
+
+func main() {
+
+	firstArr := []string{"a", "b", "c", "d", "f"}
+	secondArr := []string{"c", "d", "e", "f", "a", "b"}
+
+	fmt.Println("intersection is: ", getIntersection(firstArr, secondArr))
+}
