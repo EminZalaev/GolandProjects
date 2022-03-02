7fe98d075e9e0839958230cc683ec5f72c98ac39
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-02 18:57:03 +0300

    dev6

--- dev6/main.go
+++ dev6/main.go
@@ -1,1 +1,60 @@
-package dev6
+//Реализовать все возможные способы остановки выполнения горутины.
+package main
+
+import (
+	"sync"
+)
+
+// Через канал
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	ch := make(chan int)
+	go func() {
+		for {
+			foo, ok := <-ch
+			if !ok {
+				println("done")
+				wg.Done()
+				return
+			}
+			println(foo)
+		}
+	}()
+	ch <- 1
+	ch <- 2
+	ch <- 3
+	close(ch)
+
+	wg.Wait()
+}
+
+//  Прокидываем контекст
+//func main() {
+//	forever := make(chan struct{})
+//	ctx, cancel := context.WithCancel(context.Background())
+//
+//	go func(ctx context.Context) {
+//		for {
+//			select {
+//			case <-ctx.Done():  // if cancel() execute
+//				forever <- struct{}{}
+//				return
+//			default:
+//				fmt.Println("for loop")
+//			}
+//
+//			time.Sleep(500 * time.Millisecond)
+//		}
+//	}(ctx)
+//
+//	go func() {
+//		time.Sleep(3 * time.Second)
+//		cancel()
+//	}()
+//
+//	<-forever
+//	fmt.Println("finish")
+//}
