66af4d2e70bd5c0757c2135167fcc7fce3d95689
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-03 12:48:52 +0300

    dev14

--- dev14/main.go
+++ dev14/main.go
@@ -1,1 +1,30 @@
-package dev14
+//Разработать программу, которая в рантайме способна определить тип переменной:
+//int, string, bool, channel из переменной типа interface{}.
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+//Пакет Reflect реализует отражение во время выполнения, позволяя программе
+//манипулировать объектами произвольного типа. Типичное использование состоит в том, чтобы взять
+//значение со статическим интерфейсом типа {}
+//и извлечь информацию о его динамическом типе, вызвав TypeOf, который возвращает Type.
+
+func main() {
+	valueStr := "string"
+	valueInt := 10
+	valueBool := true
+	valueCh := make(chan int)
+
+	fmt.Println(ReflectDeterminateType(valueStr))
+	fmt.Println(ReflectDeterminateType(valueInt))
+	fmt.Println(ReflectDeterminateType(valueBool))
+	fmt.Println(ReflectDeterminateType(valueCh))
+}
+
+func ReflectDeterminateType(value interface{}) reflect.Type {
+	return reflect.TypeOf(value)
+}
