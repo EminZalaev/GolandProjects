cfb6d7d82dfca35b644e716f4f6f92918803a2af
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-03 13:09:36 +0300

    dev07

--- dev07/main.go
+++ dev07/main.go
@@ -1,1 +1,33 @@
-package dev07
+//Реализовать конкурентную запись данных в map
+package main
+
+// Мы должны взять блокировку записи на них, если мы будем обращаться
+//к ним одновременно. Используем sync.Mutex.
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var (
+		hash  = make(map[int]int)
+		mutex sync.Mutex
+		wg    sync.WaitGroup
+	)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(index int) {
+			mutex.Lock()
+			for j := 0; j < 5; j++ {
+				hash[index]++
+			}
+			defer mutex.Unlock()
+			defer wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println(hash)
+}
