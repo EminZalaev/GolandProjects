9b376efec824ce17e12e4b8cba2e24cad4114795
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-03 23:56:39 +0300

    dev02

--- dev02/main.go
+++ dev02/main.go
@@ -5,22 +5,27 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
+	"sync"
 )
 
-func calculate(param [5]int16) { //функция для расчета квадратов
-	for i := 0; i < 5; i++ { //используем цикл для возведения каждого элемента в квадрат
-		param[i] = param[i] * param[i] //и сразу же выводим
-		fmt.Println(param[i])
-		amt := time.Duration(rand.Intn(250)) //ставим рандомную задержку
-		time.Sleep(time.Millisecond * amt)
-	}
+func main() {
+	numbers := []int{1, 2, 3, 4, 5, 6}
+	PrintSquares(numbers...)
 }
 
-func main() {
-	array := [5]int16{2, 4, 6, 8, 10} //добавляем данный нам по условию массив
-	go calculate(array)               //выполняем конкурентную функцию
-	var input string                  //добавление переменной для stdout
-	fmt.Scanln(&input)                //вывод квадратов через stdout
+func PrintSquares(numbers ...int) {
+	var wg sync.WaitGroup
+	wg.Add(len(numbers))
+
+	for _, number := range numbers {
+		go func(n int) {
+			defer wg.Done()
+			fmt.Println(Square(n))
+		}(number)
+	}
+	wg.Wait()
+}
+
+func Square(number int) int {
+	return number * number
 }
