13a52f4fcdb51d007ddc6406c83b222ac15e41b5
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-04 09:53:14 +0300

    dev04 and dev09

--- dev09/main.go
+++ dev09/main.go
@@ -1,1 +1,24 @@
-package dev09
+package main
+
+import "fmt"
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	array := []int{1, 2, 3, 4, 5, 6}
+	go func() {
+		for i := 0; i < len(array); i++ {
+			ch1 <- array[i]
+		}
+		close(ch1)
+	}()
+	go func() {
+		for i := 0; i < len(array); i++ {
+			ch2 <- array[i] * 2
+		}
+		close(ch2)
+	}()
+	for x := range ch2 {
+		fmt.Println(x)
+	}
+}
