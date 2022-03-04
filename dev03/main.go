f1f1405f4a427c9aa7e303d92d620a27f886ffb6
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-04 09:34:20 +0300

    dev03

--- dev03/main.go
+++ dev03/main.go
@@ -3,25 +3,26 @@
 
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
-func calculate(param [5]int16) { //функция для расчета квадратов
-	var sum int16
-	for i := 0; i < 5; i++ { //используем цикл для возведения каждого элемента в квадрат
-		sum += param[i] * param[i]           //и суммируем их
-		amt := time.Duration(rand.Intn(250)) //ставим рандомную задержку
-		time.Sleep(time.Millisecond * amt)
+func sumCalculate(numbers ...int) int {
+	ch := make(chan int)
+	defer close(ch)
+
+	for _, number := range numbers {
+		go func(n int) {
+			ch <- n * n
+		}(number)
 	}
-	fmt.Println(sum)
+
+	sum := 0
+	for i := 0; i < len(numbers); i++ {
+		sum += <-ch
+	}
+	return sum
 }
 
 func main() {
-	array := [5]int16{2, 4, 6, 8, 10} //добавляем данный нам по условию массив
-	go calculate(array)               //выполняем конкурентную функцию
-	var input string                  //добавление переменной для stdout
-	fmt.Scanln(&input)                //вывод квадратов через stdout
+	data := []int{2, 4, 6, 8, 10}
+	fmt.Println(sumCalculate(data...))
 }
