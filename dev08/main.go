a964cb6059ecda5db0f4e1e7e80434b052076513
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-04 09:14:39 +0300

    dev08

--- dev08/main.go
+++ dev08/main.go
@@ -1,1 +1,25 @@
-package dev08
+//Дана переменная int64. Разработать программу которая устанавливает i-й бит в 1 или 0.
+package main
+
+import (
+	"fmt"
+)
+
+func bitToOne(number int64, index int) int64 {
+	newNumber := number | (1 << index)
+	return newNumber
+}
+
+func bitToZero(number int64, index int) int64 {
+	newNumber := number &^ (1 << index)
+	return newNumber
+}
+
+func main() {
+	var testNumber1 int64 = 64
+	var testNumber2 int64 = 63
+	fmt.Printf("Первое число: %b\n", testNumber1)
+	fmt.Printf("Устанавливаем второй бит в 1 : %b \n", bitToOne(testNumber1, 2))
+	fmt.Printf("Второе число: %b\n", testNumber2)
+	fmt.Printf("Устанавливаем второй бит в 0 : %b \n", bitToZero(testNumber2, 2))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/EminZalaev/SecondTrialForSamurai
+
+go 1.18
+
+require github.com/imroc/biu v0.0.0-20170329141542-0376ce6761c0
